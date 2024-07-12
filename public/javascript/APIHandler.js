1674ea07f1a077c0b7122d2737649408d04244e6
MistaKitty <tiago.abreu.laibacas@gmail.com>
2024-07-12 21:31:11 +0100

    lab solved

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,60 +1,28 @@
+
+
 class APIHandler {
-  constructor(baseUrl) {
+  constructor (baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList() {
-    axios
-      .get(`this.BASE_URL/characters`)
-      .then((response) => {
-        console.log(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+  getFullList () {
+    return axios.get(`${this.BASE_URL}/characters`);
   }
 
-  getOneRegister(characterId) {
-    axios
-      .get(`this.BASE_URL/characters/${characterId}`)
-      .then((response) => {
-        console.log(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+  getOneRegister (characterId) {
+    return axios.get(`${this.BASE_URL}/characters/${characterId}`);
   }
 
-  createOneRegister(characterInfo) {
-    axios
-      .post(`this.BASE_URL/characters`, characterInfo)
-      .then((response) => {
-        console.log(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+  createOneRegister (characterInfo) {
+    return axios.post(`${this.BASE_URL}/characters`, characterInfo);
   }
 
-  updateOneRegister(characterId, characterInfo) {
-    axios
-      .patch(`this.BASE_URL/characters/${characterId}`, characterInfo)
-      .then((response) => {
-        console.log(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+  updateOneRegister (characterId, characterInfo) {
+    return axios.put(
+      `${this.BASE_URL}/characters/${characterId}`, characterInfo);
   }
 
-  deleteOneRegister(characterId) {
-    axios
-      .delete(`this.BASE_URL/characters/${characterId}`)
-      .then((response) => {
-        console.log(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+  deleteOneRegister (characterId) {
+    return axios.delete(`${this.BASE_URL}/characters/${characterId}`);
   }
 }
