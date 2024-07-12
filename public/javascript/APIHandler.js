0a4a7bf7edd11fbe4c2e66775af9139762404fd3
MistaKitty <tiago.abreu.laibacas@gmail.com>
2024-07-12 18:44:51 +0100

    Part of API

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,60 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  getFullList() {
+    axios
+      .get(`this.BASE_URL/characters`)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
-  getOneRegister () {
-
+  getOneRegister(characterId) {
+    axios
+      .get(`this.BASE_URL/characters/${characterId}`)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
-  createOneRegister () {
-
+  createOneRegister(characterInfo) {
+    axios
+      .post(`this.BASE_URL/characters`, characterInfo)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
-  updateOneRegister () {
-
+  updateOneRegister(characterId, characterInfo) {
+    axios
+      .patch(`this.BASE_URL/characters/${characterId}`, characterInfo)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister(characterId) {
+    axios
+      .delete(`this.BASE_URL/characters/${characterId}`)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 }
